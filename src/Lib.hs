39f658bdb2a935bf3584c1a20b5f4dec6569be17
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-01-27 14:12:00 +0900

    add endpoints

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,7 +9,15 @@ import Data.Time.Calendar
 import GHC.Generics (Generic)
 import Network.Wai (Application)
 import Network.Wai.Handler.Warp (run)
-import Servant (Get, JSON, Proxy (..), Server, serve, (:>))
+import Servant
+  ( Get,
+    JSON,
+    Proxy (..),
+    Server,
+    serve,
+    type (:<|>) (..),
+    type (:>),
+  )
 
 data User = User
   { name :: String,
@@ -21,22 +29,28 @@ data User = User
 
 instance ToJSON User
 
-users1 :: [User]
-users1 =
-  [ User "Isaac Newton" 372 "issac@netton.co.uk" (fromGregorian 1683 3 1),
-    User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)
-  ]
+issac :: User
+issac = User "Isaac Newton" 372 "issac@netton.co.uk" (fromGregorian 1683 3 1)
 
-type UserAPI1 = "users" :> Get '[JSON] [User]
+albert :: User
+albert = User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)
 
-server1 :: Server UserAPI1
-server1 = return users1
+users :: [User]
+users = [issac, albert]
 
-userAPI :: Proxy UserAPI1
+type UserAPI =
+  "users" :> Get '[JSON] [User]
+    :<|> "albert" :> Get '[JSON] User
+    :<|> "issac" :> Get '[JSON] User
+
+server :: Server UserAPI
+server = return users :<|> return albert :<|> return issac
+
+userAPI :: Proxy UserAPI
 userAPI = Proxy
 
 app1 :: Application
-app1 = serve userAPI server1
+app1 = serve userAPI server
 
 runServant :: IO ()
 runServant = run 5000 app1
